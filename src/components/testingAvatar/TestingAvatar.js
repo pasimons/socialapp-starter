d321269af52cecb7cd2c50feb8fdfe03f3a9689d
Lesa Ducksworth <lducks67@gmail.com>
2020-09-30 00:04:10 -0500

    partially updates this branch

---
+++ src/components/testingAvatar/TestingAvatar.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Avatar } from 'antd';
+import { UserOutlined } from '@ant-design/icons';
+ReactDOM.render(
+  <>
+    <Avatar icon={<UserOutlined />} />
+    <Avatar>U</Avatar>
+    <Avatar size={40}>USER</Avatar>
+    <Avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />
+    <Avatar
+      style={{
+        color: '#f56a00',
+        backgroundColor: '#fde3cf',
+      }}
+    >
+      U
+    </Avatar>
+    <Avatar
+      style={{
+        backgroundColor: '#87d068',
+      }}
+      icon={<UserOutlined />}
+    />
+  </>,
+  mountNode,
+);
